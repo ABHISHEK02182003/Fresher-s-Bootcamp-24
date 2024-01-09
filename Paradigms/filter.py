d9285514c587cfd25c37559cd4f6d4b32366ea66
ABHISHEK02182003 <156063888+ABHISHEK02182003@users.noreply.github.com>
2024-01-09 15:07:26 +0530

    Update filter.py

--- Paradigms/filter.py
+++ Paradigms/filter.py
@@ -1,5 +1,5 @@
-def filter_strings(strings, check_criteria):
-    return [string for string in strings if check_criteria(string)]
+def filter_strings(strings, checking_criteria):
+    return [string for string in strings if checking_criteria(string)]
 
 def display_result_on_console(array_of_results):
     for elements in array_of_results:
