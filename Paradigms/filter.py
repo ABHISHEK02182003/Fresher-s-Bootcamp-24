b9191029d2a52ecd258cd8635cac1a8d7a9d10d8
ABHISHEK02182003 <156063888+ABHISHEK02182003@users.noreply.github.com>
2024-01-09 15:29:34 +0530

    Update filter.py

--- Paradigms/filter.py
+++ Paradigms/filter.py
@@ -8,6 +8,10 @@ def display_result_on_console(array_of_results):
 def check_starting_char_if_a(string):
     return string[0] == "A"
 
+def check_ending_char_if_i(string):
+    return string[0] == "i"
+
 if __name__ == "__main__":
     sample_array_of_strings = ["Abhishek", "Sameer Trivedi", "Sankhanil Nayek", "Ishan Madan", "Arravelly Keerthi "]
     display_result_on_console(filter_strings(sample_array_of_strings, check_starting_char_if_a))
+    display_result_on_console(filter_strings(sample_array_of_strings, check_ending_char_if_i))
