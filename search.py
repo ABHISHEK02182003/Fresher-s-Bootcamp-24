a09b3f65d197aec65076333169ab818db19f4690
ABHISHEK02182003 <156063888+ABHISHEK02182003@users.noreply.github.com>
2024-01-09 13:53:04 +0530

    Update search.py

--- search.py
+++ search.py
@@ -1,15 +1,19 @@
 def search_element(arr, target):
     for index, element in enumerate(arr):
         if element == target:
-            return index 
-    return -1  
+            return display_result(target, index)
+    return display_result(target, -1)
 
-my_array = [1, 3, 5, 7, 9, 11]
-target_element = 7
+def display_result(target, k):
+    if k != -1:
+        print(f"Element {target} found at index {k}")
+    else:
+        print(f"Element {target} not found in the array")
 
-result = search_element(my_array, target_element)
+def main():
+    my_array = [1, 3, 5, 7, 9, 11]
+    target_element = 7
+    search_element(my_array, target_element)
 
-if result != -1:
-    print(f"Element {target_element} found at index {result}")
-else:
-    print(f"Element {target_element} not found in the array")
+if __name__ == "__main__":
+    main()
