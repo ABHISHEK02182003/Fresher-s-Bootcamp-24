837c23129a582d1b90969cbaa820f3200f7933c6
ABHISHEK02182003 <156063888+ABHISHEK02182003@users.noreply.github.com>
2024-01-09 14:12:19 +0530

    Update search.py

--- Paradigms/search.py
+++ Paradigms/search.py
@@ -12,7 +12,7 @@ def display_result(target, k):
 
 def main():
     my_array = [1, 3, 5, 7, 9, 11]
-    target_element = 7
+    sample_target_element = 7
     search_element(my_array, target_element)
 
 if __name__ == "__main__":
