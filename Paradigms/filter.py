8275ea562bf3300dc0e6e662af5381f5ec88080a
ABHISHEK02182003 <156063888+ABHISHEK02182003@users.noreply.github.com>
2024-01-09 14:47:45 +0530

    Update filter.py

--- Paradigms/filter.py
+++ Paradigms/filter.py
@@ -1,12 +1,1 @@
-from typing import List, Callable
-
-def filter_elements(arr: List[int], criteria: Callable[[int], bool]) -> List[int]:
-    result = [ele for ele in arr if criteria(ele)]
-    return result
-
-def main():
-    print("Execution Successful")
-
-if __name__ == "__main__":
-    main()
 
