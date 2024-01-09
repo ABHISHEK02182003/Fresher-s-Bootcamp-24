91aab87ea4268049359d43b77a696717a40ca876
ABHISHEK02182003 <156063888+ABHISHEK02182003@users.noreply.github.com>
2024-01-09 14:59:40 +0530

    Update filter.py

--- Paradigms/filter.py
+++ Paradigms/filter.py
@@ -1,1 +1,14 @@
+def filter_strings(strings, check_predicate):
+    return [string for string in strings if check_predicate(string)]
+
+def display_result_on_console(array_of_results):
+    for elements in array_of_results:
+        print(elements)
+
+def check_starting_string_if_a(string):
+    return string[0] == "A"
+
+if __name__ == "__main__":
+    sample_array_of_strings = ["Abhishek", "Sameer Trivedi", "Sankhanil Nayek", "Abhinav Modi", "Arravelly Keerthi "]
+    display_strings(filter_strings(strings, check_starting_string_if_a))
 
