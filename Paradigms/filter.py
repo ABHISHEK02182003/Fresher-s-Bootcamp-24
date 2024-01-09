97e6bc3bfddbfb8a9e50cc65885780c069d04bc9
ABHISHEK02182003 <156063888+ABHISHEK02182003@users.noreply.github.com>
2024-01-09 16:14:11 +0530

    Update filter.py

--- Paradigms/filter.py
+++ Paradigms/filter.py
@@ -1,18 +1,27 @@
-def filter_strings(strings, checking_criteria_fn):
-    return [string for string in strings if checking_criteria_fn(string)]
+def filter_strings(strings, checking_criteria):
+    return [string for string in strings if checking_criteria(string)]
 
 def display_result_on_console(array_of_results):
     for elements in array_of_results:
         print(elements)
-    print("")
 
-def check_starting_char_if_a(string):
-    return string[0] == "A"
+def create_starting_char_checker(char):
+    def check_starting_char(string):
+        return string[0].lower() == char.lower()
+    return check_starting_char
 
-def check_ending_char_if_i(string):
-    return string[-1] == "i"
+def create_ending_char_checker(char):
+    def check_ending_char(string):
+        return string[-1].lower() == char.lower()
+    return check_ending_char
 
-if __name__ == "__main__":
-    sample_array_of_strings = ["Abhishek", "Sameer Trivedi", "Sankhanil Nayek", "Ishan Madan", "Arravelly Keerthi"]
-    display_result_on_console(filter_strings(sample_array_of_strings, check_starting_char_if_a)
-    display_result_on_console(filter_strings(sample_array_of_strings, check_ending_char_if_i)
+if _name_ == "_main_":
+    sample_array_of_strings = ["Abhishek", "Sameer Trivedi", "Sankhanil Nayek", "Ishan Madan", "Arravelly Keerthi "]
+
+    for char in ['a', 'd', 's', 'i']:
+        starting_char_check = create_starting_char_checker(char)
+        display_result_on_console(filter_strings(sample_array_of_strings, starting_char_check))
+
+    for char in ['a', 'd', 's', 'i']:
+        ending_char_check = create_ending_char_checker(char)
+        display_result_on_console(filter_strings(sample_array_of_strings, ending_char_check))
