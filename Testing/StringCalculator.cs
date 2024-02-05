f8fba664b2e86b1a4fb53aca5b7a00b2e59ef544
ABHISHEK02182003 <156063888+ABHISHEK02182003@users.noreply.github.com>
2024-02-05 15:00:31 +0530

    Update StringCalculator.cs

--- Testing/StringCalculator.cs
+++ Testing/StringCalculator.cs
@@ -1,75 +1,112 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-public class StringCalculator
+public interface IInputValidator
 {
-    public static int Add(string numbers)
-    {
-        if (string.IsNullOrEmpty(numbers))
-            return 0;
+    void ValidateInput(string numbers);
+}
 
-        CheckForInvalidInput(numbers);
+public interface IDelimiterProvider
+{
+    char[] GetDelimiters(string numbers);
+}
 
-        char[] delimiters = GetDelimiters(numbers);
-        string[] numbersArray = GetNumbersArray(numbers, delimiters);
+public interface INumberParser
+{
+    int[] ParseNumbers(string numbers);
+}
 
-        CheckForNegativeNumbers(numbersArray);
+public interface INegativeNumberChecker
+{
+    void CheckForNegativeNumbers(int[] numbers);
+}
 
-        return numbersArray.Where(x => int.Parse(x) <= 1000).Sum(x => int.Parse(x));
-    }
-
-    private static void CheckForInvalidInput(string numbers)
+public class InputValidator : IInputValidator
+{
+    public void ValidateInput(string numbers)
     {
         if (numbers.EndsWith(",") || numbers.EndsWith("\n"))
         {
             throw new ArgumentException("Invalid input: Trailing comma or newline");
         }
     }
+}
 
-    private static char[] GetDelimiters(string numbers)
+public class DelimiterProvider : IDelimiterProvider
+{
+    public char[] GetDelimiters(string numbers)
     {
         char[] defaultDelimiters = { ',', '\n' };
 
         if (numbers.StartsWith("//"))
         {
-            int delimiterEndIndex = numbers.IndexOf('\n');
-            string delimiterSection = numbers.Substring(2, delimiterEndIndex - 2);
-
-            if (delimiterSection.StartsWith("[") && delimiterSection.EndsWith("]"))
-            {
-                var delimiters = new List<string>(delimiterSection
-                    .TrimStart('[')
-                    .TrimEnd(']')
-                    .Split("]["));
-
-                return delimiters.SelectMany(d => d.ToCharArray()).ToArray();
-            }
-            else
-            {
-                return new char[] { delimiterSection[0] };
-            }
+            return GetCustomDelimiters(numbers);
         }
 
         return defaultDelimiters;
     }
 
-    private static string[] GetNumbersArray(string numbers, char[] delimiters)
+    private char[] GetCustomDelimiters(string numbers)
     {
-        string numbersSection = numbers;
-        if (numbers.StartsWith("//"))
-            numbersSection = numbers.Substring(numbers.IndexOf('\n') + 1);
+        int delimiterStartIndex = numbers.IndexOf("[") + 1;
+        int delimiterEndIndex = numbers.IndexOf("]");
 
-        return numbersSection.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+        if (delimiterStartIndex > 0 && delimiterEndIndex > delimiterStartIndex)
+        {
+            string customDelimiterSection = numbers.Substring(delimiterStartIndex, delimiterEndIndex - delimiterStartIndex);
+            return customDelimiterSection.ToCharArray();
+        }
+
+        // If no valid custom delimiters are found, you can return a default delimiter
+        return new char[] { ';' }; // You can replace this with the default delimiter of your choice
+    }
+}
+
+public class NumberParser : INumberParser
+{
+    public int[] ParseNumbers(string numbers)
+    {
+        string numbersSection = GetNumbersSection(numbers);
+        return numbersSection.Split(GetDelimiters(numbers), StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
     }
 
-    private static void CheckForNegativeNumbers(string[] numbersArray)
+    private char[] GetDelimiters(string numbers)
     {
-        var negativeNumbers = numbersArray.Where(x => int.Parse(x) < 0).ToList();
+        return new DelimiterProvider().GetDelimiters(numbers);
+    }
+
+    private string GetNumbersSection(string numbers)
+    {
+        if (numbers.StartsWith("//"))
+            return numbers.Substring(numbers.IndexOf('\n') + 1);
+
+        return numbers;
+    }
+}
+
+public class NegativeNumberChecker : INegativeNumberChecker
+{
+    public void CheckForNegativeNumbers(int[] numbers)
+    {
+        var negativeNumbers = numbers.Where(x => x < 0).ToList();
 
         if (negativeNumbers.Any())
         {
             throw new ArgumentException($"Negatives not allowed: ({string.Join(", ", negativeNumbers)})");
         }
+    }
+}
+
+public static class StringCalculator
+{
+    public static int Add(string numbers)
+    {
+        if (string.IsNullOrEmpty(numbers))
+            return 0;
+
+        new InputValidator().ValidateInput(numbers);
+        int[] parsedNumbers = new NumberParser().ParseNumbers(numbers);
+        new NegativeNumberChecker().CheckForNegativeNumbers(parsedNumbers);
+
+        return parsedNumbers.Where(x => x <= 1000).Sum();
     }
 }
